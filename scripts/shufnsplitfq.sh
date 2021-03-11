379875110132191e6bf2824e08157b4fffdeb3b2
koriege <koriege@gmail.com>
2021-03-11 22:04:48 +0100

    usage update

--- scripts/shufnsplitfq.sh
+++ scripts/shufnsplitfq.sh
@@ -14,8 +14,8 @@ cat <<- EOF
 
 	OPTIONS
 	-h          | this help
-	-1 <path>   | input single or first fastq(.gz|.bz2)
-	-2 <path>   | input second mate fastq(.gz|.bz2)
+	-1 <path>   | input SE or first mate fastq(.gz|.bz2)
+	-2 <path>   | input mate pair fastq(.gz|.bz2)
 	-z          | compress output using pigz with [-t] threads (fallback: gzip)
 	-t <value>  | compression threads (default: $t)
 	-o <path>   | prefix of output fastq, extended by [1|2].(R1.|R2.)fastq(.gz)
