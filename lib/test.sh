5431e8b30dd8c32f9aee05f01039a4f224c8ada7
koriege <koriege@gmail.com>
2023-09-01 22:21:35 +0200

    +runcmd test

--- lib/test.sh
+++ lib/test.sh
@@ -1,6 +1,28 @@
 #! /usr/bin/env bash
 # (c) Konstantin Riege
 
+function test::test(){
+		function _usage(){
+		commander::print {COMMANDER[0]}<<- EOF
+			${FUNCNAME[1]} usage:
+			-h | help
+		EOF
+		return 1
+	}
+
+	local OPTIND arg tmpdir="${TMPDIR:-/tmp}"
+	declare -n _mapper_coexpression _idfiles_coexpression
+	while getopts 'h' arg; do
+		case $arg in
+			h)	_usage || true; return 0;;
+			*)	_usage;;
+		esac
+	done
+	commander::printinfo "running test::test"
+	declare -a cmd=('sleep 2')
+	commander::runcmd -v -b -i 1 -a cmd
+}
+
 function test::start(){
 	echo test::start under $BASHPID >&2
 	# cat <(test::inner; echo mut not show test::start >&2)
