3eadff8a6b057eb778c328a725817de41fa9eea7
koriege <koriege@gmail.com>
2023-10-18 14:47:56 +0200

    +output option for quantify::bamcoverage i.e. parameter change -o -> -O, and switch to deeptools for binsize==1 while maintaining order of reference sequences

--- lib/quantify.sh
+++ lib/quantify.sh
@@ -170,7 +170,7 @@ function quantify::tpm(){
 					args = parse_args(args);
 					dds = DESeqDataSetFromHTSeqCount(sampleTable=read.table(args$csv, header=F, sep=",", stringsAsFactors=F, check.names=F, quote=""), directory="", design=~1);
 					dds = estimateSizeFactors(dds);
-					vsd = rbind(colnames(dds),assay(varianceStabilizingTransformation(dds, blind=FALSE)));
+					vsd = rbind(colnames(dds),assay(varianceStabilizingTransformation(dds, blind=TRUE)));
 					ret = apply(vsd, 2, function(x){ write.table(data.frame(rownames(dds), x[2:length(x)], check.names=F), file=paste0(x[1],".vsc"), col.names=F, row.names=F, quote=F, sep="\t") });
 				'
 			CMD
@@ -189,13 +189,13 @@ function quantify::tpm(){
 }
 
 function quantify::bamcoverage(){
-	# bedg != samtools bedcov <- which is sum(histogram = samtools depth), use bedtools multicov
+	# bedg != samtools bedcov <- which is sum(histogram = samtools depth)
 	# use
 	# bedtools multicov -D -F -bams <bam>[ <bam>..] -bed <bins> > <bedg> (slow)
 	# or
 	# bamCoverage --bam $f -o <*.bw|*.bedg> -p 24 -bs <binsize> (merges adjacent bins of same coverage into up to 5000000bp bins, ignores mates with an insert >1000bp)
 	# or
-	# featureCounts from SAF formatted bins, which allows also fractional counts
+	# featureCounts from SAF formatted bins, which allows also fractional counts (slow for very small windows)
 
 	# bamCoverage                      featureCounts
 	# ...                              ...
@@ -226,11 +226,12 @@ function quantify::bamcoverage(){
 			                  0 - unstranded
 			                  1 - stranded
 			                  2 - reversely stranded
-			-o <overlap>    | of a read, that spans multiple features, which assigns it to one or multiple feature. default: 1
+			-o <outdir>     | path to
+			-O <overlap>    | of a read, that spans multiple features, which assigns it to one or multiple feature. default: 1
 			                  0   - largest
 			                  0-1 - fraction
 			                  1-N - basepairs
-			-w <windowsize> | for bins to be quantified, unless given a bed/bed-like file by -b option. default: 1
+			-w <windowsize> | for bins to be quantified, unless given a bed/bed-like file by -b option. default: 100
 			-b <bed>        | or bed-like file with 3 or 6 columns. latter for strand-specific quantification. see also -w
 			-i              | index of -b option given file is 1 and not 0 (e.g. narrowPeak, gtf, gff)
 			-f              | count reads fractional if, according to -o option, they can be assigned to multiple features (+1/n)
@@ -240,20 +241,22 @@ function quantify::bamcoverage(){
 			                  NOTE1: +1 instead of +2 is counted only if mates are unambiguously assigned to same feature, else +1,+1
 			                  NOTE2: fractional quantification is not available
 			                  NOTE3: unless -o option corresponds to basepairs, assignment is derived from the sum of both mate lengths
+			-d              | when using -w <windowsize>, use deepTools bamCoverage (ignores -O -f -u -p)
 		EOF
 		return 1
 	}
 
-	local OPTIND arg mandatory skip=false tmpdir="${TMPDIR:-/tmp}" threads overlap=1 windowsize=1 bed index=0 fractional=false unambiguous=false pairs=false
+	local OPTIND arg mandatory skip=false tmpdir="${TMPDIR:-/tmp}" threads overlap=1 windowsize=100 bed index=0 fractional=false unambiguous=false pairs=false outdir
 	declare -n _mapper_bam2bedg _strandness_bam2bedg
-	while getopts 'S:s:t:r:x:o:w:b:ifup' arg; do
+	while getopts 'S:s:t:r:x:o:O:w:b:ifup' arg; do
 		case $arg in
 			S) $OPTARG && return 0;;
 			s) $OPTARG && skip=true;;
 			t) ((++mandatory)); threads=$OPTARG;;
 			r) ((++mandatory)); _mapper_bam2bedg=$OPTARG;;
 			x) _strandness_bam2bedg=$OPTARG;;
-			o) overlap=$OPTARG;;
+			o) outdir="$OPTARG"; mkdir -p "$outdir";;
+			O) overlap=$OPTARG;;
 			w) windowsize=$OPTARG;;
 			b) bed="$OPTARG";;
 			i) index=1;;
@@ -278,82 +281,118 @@ function quantify::bamcoverage(){
 
 	commander::printinfo "convertig bam to bedg/bw"
 
-	local m f params x o
-	declare -a cmd1 cmd2 cmd3 tdirs
+	local m f params x o e odir
+	[[ $bed || $windowsize -gt 1 ]] && e="coverage" || e="pileup"
+
+	declare -a cmd1 cmd2 cmd3 cmd4 tdirs
 	for m in "${_mapper_bam2bedg[@]}"; do
 		declare -n _bams_bam2bedg=$m
 		for f in "${_bams_bam2bedg[@]}"; do
-			o="${f%.*}"
+			o="$(basename "${f%.*}")"
 			tdirs+=("$(mktemp -d -p "$tmpdir" cleanup.XXXXXXXXXX.bam2bedg)")
-
-			if [[ $bed ]]; then
-				commander::makecmd -a cmd1 -s ' ' -o "${tdirs[-1]}/regions.saf" -c {COMMANDER[0]}<<- CMD {COMMANDER[2]}<<- 'CMD'
-					samtools view -H "${_bams_bam2bedg[0]}" | sed -nE '/^@SQ/{s/.*SN:(\S+).*LN:(\S+)\s*.*/\1\t\2/p}' | helper::sort -t $threads -k1,1 > "${tdirs[-1]}/chr.sizes";
-					helper::sort -f "$bed" -t $threads -k1,1 -k2,2n -k3,3n | awk -v i=$index -v OFS='\t'
-				CMD
-					'BEGIN{i=i==1?0:1}{id=$1":"$2"-"$3; if($4){id=$4}; s="."; if($6){s=$6}; print id,$1,$2+i,$3,".",".",s}'
-				CMD
-			else
-				commander::makecmd -a cmd1 -s '|' -o "${tdirs[-1]}/regions.saf" -c {COMMANDER[0]}<<- CMD {COMMANDER[2]}<<- 'CMD'
-					samtools view -H "${_bams_bam2bedg[0]}" | sed -nE '/^@SQ/{s/.*SN:(\S+).*LN:(\S+)\s*.*/\1\t\2/p}' | helper::sort -t $threads -k1,1 > "${tdirs[-1]}/chr.sizes";
-					bedtools makewindows -w $windowsize -g "${tdirs[-1]}/chr.sizes"
-				CMD
-					awk -v OFS='\t' '{id=$1":"$2"-"$3; print id,$1,$2+1,$3,".",".","."}'
-				CMD
-			fi
-
-			params=''
-			$unambiguous || params+=" -O"
-			$fractional && params+=" --fraction"
-			params+=" $(echo $overlap | awk '{if($1==0){print "--largestOverlap"}else{print $1<1? "--fracOverlap "$1 : "--minOverlap "$1}}')"
-			if [[ $version == "old" ]]; then
-				$pairs && params+=" -p"
-			else
-				x=$(samtools view -F 4 "$f" | head -10000 | cat <(samtools view -H "$f") - | samtools view -c -f 1)
-				if [[ $x -gt 0 ]]; then
-					params+=" -p"
-					$pairs && params+=" --countReadPairs"
+			odir="$outdir/$m"
+			[[ $outdir ]] || odir="$(dirname "$f")"
+			mkdir -p "$odir"
+			if [[ $bed || $windowsize -gt 1 ]]; then
+				if [[ $bed ]]; then
+					commander::makecmd -a cmd1 -s ' ' -o "${tdirs[-1]}/regions.saf" -c {COMMANDER[0]}<<- CMD {COMMANDER[2]}<<- 'CMD'
+						samtools view -H "$f" | sed -nE '/^@SQ/{s/.*SN:(\S+).*LN:(\S+)\s*.*/\1\t\2/p}' | helper::sort -t $threads -k1,1 > "${tdirs[-1]}/chr.sizes";
+						helper::sort -f "$bed" -t $threads -k1,1 -k2,2n -k3,3n | awk -v i=$index -v OFS='\t'
+					CMD
+						'BEGIN{i=i==1?0:1}{id=$1":"$2"-"$3; if($4){id=$4}; s="."; if($6){s=$6}; print id,$1,$2+i,$3,".",".",s}'
+					CMD
+				else
+					commander::makecmd -a cmd1 -s '|' -o "${tdirs[-1]}/regions.saf" -c {COMMANDER[0]}<<- CMD {COMMANDER[2]}<<- 'CMD'
+						samtools view -H "$f" | sed -nE '/^@SQ/{s/.*SN:(\S+).*LN:(\S+)\s*.*/\1\t\2/p}' | helper::sort -t $threads -k1,1 > "${tdirs[-1]}/chr.sizes";
+						bedtools makewindows -w $windowsize -g "${tdirs[-1]}/chr.sizes"
+					CMD
+						awk -v OFS='\t' '{id=$1":"$2"-"$3; print id,$1,$2+1,$3,".",".","."}'
+					CMD
 				fi
+
+				params=''
+				$unambiguous || params+=" -O"
+				$fractional && params+=" --fraction"
+				params+=" $(echo $overlap | awk '{if($1==0){print "--largestOverlap"}else{print $1<1? "--fracOverlap "$1 : "--minOverlap "$1}}')"
+				if [[ $version == "old" ]]; then
+					$pairs && params+=" -p"
+				else
+					x=$(samtools view -F 4 "$f" | head -10000 | cat <(samtools view -H "$f") - | samtools view -c -f 1)
+					if [[ $x -gt 0 ]]; then
+						params+=" -p"
+						$pairs && params+=" --countReadPairs"
+					fi
+				fi
+
+				commander::makecmd -a cmd2 -s '|' -o "$odir/$o.$e.tpm.bed" -c {COMMANDER[0]}<<- CMD {COMMANDER[1]}<<- CMD {COMMANDER[2]}<<- 'CMD'
+					featureCounts
+						$params
+						-Q 0
+						--maxMOp 999
+						-s $(declare -p _strandness_bam2bedg | grep -q '=' && echo ${_strandness_bam2bedg["$f"]} || echo 0)
+						-T $ithreads
+						-f
+						-M
+						--ignoreDup
+						--tmpDir "${tdirs[-1]}"
+						-F SAF
+						-a "${tdirs[-1]}/regions.saf"
+						-o /dev/stdout
+						"$f"
+				CMD
+					tee -ia >(awk -v OFS='\t' 'NR>2{print \$2,\$3-1,\$4,sprintf("%d",\$7==int(\$7)?\$7:\$7+1)}' > "$odir/$o.$e.counts.bed")
+				CMD
+					awk '
+						NR>2{
+							o[NR-3]=$2"\t"$3-1"\t"$4;
+							v[NR-3]=sprintf("%d",$7==int($7)?$7:$7+1)/$6;
+							sum=sum+v[NR-3];
+						}
+						END{
+							sum=sum/1000000;
+							for(i=0;i<=NR-3;i++){
+								print o[i]"\t"v[i]/sum;
+							}
+						}
+					'
+				CMD
+			else
+				commander::makecmd -a cmd1 -s '|' -c {COMMANDER[0]}<<- CMD {COMMANDER[1]}<<- 'CMD' {COMMANDER[2]}<<- CMD
+					samtools view -H "$f"
+				CMD
+					sed -nE '/^@SQ/{s/.*SN:(\S+).*LN:(\S+)\s*.*/\1\t\2/p}'
+				CMD
+					helper::sort -t $threads -k1,1 > "${tdirs[-1]}/chr.sizes"
+				CMD
+
+				commander::makecmd -a cmd3 -s ';' -c {COMMANDER[0]}<<- CMD {COMMANDER[1]}<<- CMD {COMMANDER[2]}<<- CMD
+					rm -f "$odir/$o.$e.counts.bed"
+				CMD
+					mapfile -t chr < <(cut -f 1 "${tdirs[-1]}/chr.sizes")
+				CMD
+					for r in "\${chr[@]}"; do
+						bamCoverage -r \$r -b "$f" -bs $windowsize -p $threads --normalizeUsing None --outFileFormat bedgraph -o "${tdirs[-1]}/$e.counts.bed";
+						cat "${tdirs[-1]}/$e.counts.bed" >> "$odir/$o.$e.counts.bed";
+					done
+				CMD
+
+				commander::makecmd -a cmd3 -s ';' -c {COMMANDER[0]}<<- CMD {COMMANDER[1]}<<- CMD {COMMANDER[2]}<<- CMD
+					rm -f "$odir/$o.$e.tpm.bed"
+				CMD
+					mapfile -t chr < <(cut -f 1 "${tdirs[-1]}/chr.sizes")
+				CMD
+					for r in "\${chr[@]}"; do
+						bamCoverage -r \$r -b "$f" -bs $windowsize -p $threads --normalizeUsing CPM --outFileFormat bedgraph -o "${tdirs[-1]}/$e.tpm.bed";
+						cat "${tdirs[-1]}/$e.tpm.bed" >> "$odir/$o.$e.tpm.bed";
+					done
+				CMD
 			fi
 
-			commander::makecmd -a cmd2 -s '|' -o "$o.coverage.tpm.bed" -c {COMMANDER[0]}<<- CMD {COMMANDER[1]}<<- CMD {COMMANDER[2]}<<- 'CMD'
-				featureCounts
-					$params
-					-Q 0
-					--maxMOp 999
-					-s $(declare -p _strandness_bam2bedg | grep -q '=' && echo ${_strandness_bam2bedg["$f"]} || echo 0)
-					-T $ithreads
-					-f
-					-M
-					--ignoreDup
-					--tmpDir "${tdirs[-1]}"
-					-F SAF
-					-a "${tdirs[-1]}/regions.saf"
-					-o /dev/stdout
-					"$f"
+			commander::makecmd -a cmd4 -s ';' -c {COMMANDER[0]}<<- CMD
+				bedGraphToBigWig "$odir/$o.$e.counts.bed" "${tdirs[-1]}/chr.sizes" "$odir/$o.$e.counts.bw"
 			CMD
-				tee -ia >(awk -v OFS='\t' 'NR>2{print \$2,\$3-1,\$4,sprintf("%d",\$7==int(\$7)?\$7:\$7+1)}' > "$o.coverage.counts.bed")
-			CMD
-				awk '
-					NR>2{
-						o[NR-3]=$2"\t"$3-1"\t"$4;
-						v[NR-3]=sprintf("%d",$7==int($7)?$7:$7+1)/$6;
-						sum=sum+v[NR-3];
-					}
-					END{
-						sum=sum/1000000;
-						for(i=0;i<=NR-3;i++){
-							print o[i]"\t"v[i]/sum;
-						}
-					}
-				'
-			CMD
-
-			commander::makecmd -a cmd3 -s ';' -c {COMMANDER[0]}<<- CMD
-				bedGraphToBigWig "$o.coverage.counts.bed" "${tdirs[-1]}/chr.sizes" "$o.coverage.counts.bw"
-			CMD
-			commander::makecmd -a cmd3 -s ';' -c {COMMANDER[0]}<<- CMD
-				bedGraphToBigWig "$o.coverage.tpm.bed" "${tdirs[-1]}/chr.sizes" "$o.coverage.tpm.bw"
+			commander::makecmd -a cmd4 -s ';' -c {COMMANDER[0]}<<- CMD
+				bedGraphToBigWig "$odir/$o.$e.tpm.bed" "${tdirs[-1]}/chr.sizes" "$odir/$o.$e.tpm.bw"
 			CMD
 		done
 	done
@@ -362,10 +401,12 @@ function quantify::bamcoverage(){
 		commander::printcmd -a cmd1
 		commander::printcmd -a cmd2
 		commander::printcmd -a cmd3
+		commander::printcmd -a cmd4
 	else
 		commander::runcmd -v -b -i 1 -a cmd1
 		commander::runcmd -c subread -v -b -i $instances -a cmd2
-		commander::runcmd -v -b -i $threads -a cmd3
+		commander::runcmd -c deeptools -v -b -i 1 -a cmd3
+		commander::runcmd -v -b -i $threads -a cmd4
 	fi
 
 	return 0
