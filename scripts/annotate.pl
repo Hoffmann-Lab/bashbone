dda78911b1261fd8dac78daf8244919897dce561
koriege <koriege@gmail.com>
2023-12-12 10:12:53 +0100

    ps/bed/deseq out annotation made more universally usable

--- scripts/annotate.pl
+++ scripts/annotate.pl
@@ -5,9 +5,9 @@ use warnings;
 use feature ":5.10";
 
 if ($#ARGV < 3){
-	say "usage: annotate.pl <gtf.descr|0> <gtf|0> <feature> <bed|deseq.tsv|heatmap.ps> [<bed|deseq.tsv|heatmap.ps> ..]";
-	say "or"
-	say "usage: annotate.pl <gtf.descr|0> <gtf|0> <feature> <deseq.tsv> [tpm]";
+	say "usage: annotate.pl <gtf.descr|0> <gtf|0> <feature> <bed|deseq.tsv|heatmap.ps>";
+	say "or";
+	say "usage: annotate.pl <gtf.descr|0> <gtf|0> <feature> <deseq.tsv> <tpm|vsc|zscores> [extension]";
 	say "0 or gtf.descr => 4 tab seperated columns: geneID geneName biotype description";
 	say "0 or gtf => parsed for gene_id gene_name gene_biotype";
 	say "feature => e.g. exon -> gtf parsed for exon_id exon_name exon_biotype";
@@ -54,10 +54,9 @@ if ($ARGV[1]){
 	close I;
 }
 
-my $files = $#ARGV == 3 ? 3 : $#ARGV-1;
 my %tpm;
-if ($files < $#ARGV){
-	open I,"<$ARGV[-1]" or die $!;
+if ($#ARGV > 3){
+	open I,"<$ARGV[4]" or die $!;
 	while(<I>){
 		chomp;
 		my @l=split/\t/;
@@ -67,60 +66,62 @@ if ($files < $#ARGV){
 	$tpm{NA}=$tpm{id}=~s/\S+/NA/gr;
 }
 
-for (3..$files){
-	my $f=$ARGV[$_];
-	my @o=split/\./,$f;
-	my $e=$o[-1];
+my $f=$ARGV[3];
+my @o=split/\./,$f;
+my $e=$o[-1];
+if ($#ARGV > 4){
+	$o[-1]=$ARGV[5].".annotated";
+} else {
 	$o[-1]="annotated";
-	push @o,$e;
-	my $o=join".",@o;
-	open I,"<$f" or die $!;
-	open O,">$o" or die $!;
-	my $ps;
-	my $deseq;
-	while(<I>){
-		chomp;
-		my @l=split/\t/;
-		if ($.==1){
-			if (/PS-Adobe/){
-				$ps=1;
-				say O;
-				next;
-			} elsif(/log2FoldChange/){
-				$deseq=1;
-				if ($files < $#ARGV){
-					say O join"\t",(@l,"name","biotype","description",$tpm{id});
-				} else {
-					say O join"\t",(@l,"name","biotype","description");
-				}
-				next;
-			} # else: not next!
-		}
-		# do splits by '@' in case of merged gtf with ids of type feature@subfeature@subfeature...
-		if ($ps) {
-			if (/\((\S+)(\s\+|\s-)*\)\s+(0|0\.25)\s+0\s+t$/) {
-				my $n = $mps{$1};
-				$n = $mps{(split/\@/,$1)[-1]} unless $n;
-				s/$1/$n/ if $n;
-			}
-			say O;
-		} elsif($deseq) {
-			my $n = $m{$l[0]};
-			$n = $m{(split/\@/,$l[0])[-1]} unless $n;
-			$n = join"\t",('NA','NA','NA') unless $n;
-			if ($files < $#ARGV){
-				my $t = $tpm{$l[0]};
-				$t = $tpm{NA} unless $t;
-				$n .= "\t$t";
-			}
-			say O join"\t",(@l,$n);
-		} else {
-			my $n = $m{$l[3]};
-			$n = $m{(split/\@/,$l[3])[-1]} unless $n;
-			$n = join"\t",('NA','NA','NA') unless $n;
-			say O join"\t",(@l,$n);
-		}
-	}
-	close I;
-	close O;
 }
+push @o,$e;
+my $o=join".",@o;
+open I,"<$f" or die $!;
+open O,">$o" or die $!;
+my $ps;
+my $deseq;
+while(<I>){
+	chomp;
+	my @l=split/\t/;
+	if ($.==1){
+		if (/PS-Adobe/){
+			$ps=1;
+			say O;
+			next;
+		} elsif(/log2FoldChange/){
+			$deseq=1;
+			if ($#ARGV > 3){
+				say O join"\t",(@l,"name","biotype","description",$tpm{id});
+			} else {
+				say O join"\t",(@l,"name","biotype","description");
+			}
+			next;
+		} # else: not next!
+	}
+	# do splits by '@' in case of merged gtf with ids of type feature@subfeature@subfeature...
+	if ($ps) {
+		if (/\((\S+)(\s\+|\s-)*\)\s+(0|0\.25)\s+0\s+t$/) {
+			my $n = $mps{$1};
+			$n = $mps{(split/\@/,$1)[-1]} unless $n;
+			s/$1/$n/ if $n;
+		}
+		say O;
+	} elsif($deseq) {
+		my $n = $m{$l[0]};
+		$n = $m{(split/\@/,$l[0])[-1]} unless $n;
+		$n = join"\t",('NA','NA','NA') unless $n;
+		if ($#ARGV > 3){
+			my $t = $tpm{$l[0]};
+			$t = $tpm{NA} unless $t;
+			$n .= "\t$t";
+		}
+		say O join"\t",(@l,$n);
+	} else {
+		my $n = $m{$l[3]};
+		$n = $m{(split/\@/,$l[3])[-1]} unless $n;
+		$n = join"\t",('NA','NA','NA') unless $n;
+		say O join"\t",(@l,$n);
+	}
+}
+close I;
+close O;
