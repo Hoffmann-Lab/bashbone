1653df2009fe7a1454cb889028931d3e93ca75d2
koriege <koriege@gmail.com>
2024-04-12 19:33:34 +0200

    bgzip for faster compression, decompression according to disk type and available indices (+rapidgzip), +parallel apply on gipz compressed or uncompressed files

--- lib/helper.sh
+++ lib/helper.sh
@@ -8,27 +8,171 @@ function helper::pgzip(){
 			-f <infile>  | path to. else stdin
 			-o <outfile> | path to
 			-t <threads> | number of
-			-b           | block zip via bgzip
+			-p           | use slower pigz instead of bgzip to lower disk footprint by 5%
 		EOF
 		return 1
 	}
 
-	local OPTIND arg threads=1 f o tool="pigz -p"
-	while getopts 'f:t:o:b' arg; do
+	local OPTIND arg threads=1 f o tool="bgzip"
+	while getopts 'f:t:o:p' arg; do
 		case $arg in
-			f) f="$OPTARG";;
-			o) o="$OPTARG"; mkdir -p "$(dirname "$o")";;
-			t) threads=$OPTARG;;
-			b) tool="bgzip -@";;
-			*) _usage;;
+			f)	f="$OPTARG";;
+			o)	o="$OPTARG"; mkdir -p "$(dirname "$o")";;
+			t)	threads=$OPTARG;;
+			p)	tool="pigz";;
+			*)	_usage;;
 		esac
 	done
 	[[ ! $f && ! $o ]] && { _usage || return 0; }
 	[[ ! $f ]] && f=/dev/stdin
 	[[ ! $o ]] && o="$f.gz"
 
-	$tool $threads -k -c "$f" | tee -i "$o" | gztool -v 0 -f -i -x -C -I "${o%.*}.gzi"
+	if [[ $tool == "bgzip" && -x "$(command -v bgzip)" ]]; then
+		# compression level changed from v1.16 to ~match gzip size at cost of throughput (still being faster than pigz)
+		tool+=" -l $(bgzip --version | head -1 | awk 'NF>1.15{print 5; exit}{print 6}') -@"
+	else
+		tool="pigz -p"
+	fi
 
+	if [[ -x "$(command -v rapidgzip)" ]]; then
+		$tool $threads -k -c "$f" | tee -i >(gztool -v 0 -f -i -x -C -I "${o%.*}.gzi") >(rapidgzip --export-index "${o%.*}.rgzi") > "$o" | cat
+	else
+		$tool $threads -k -c "$f" | tee -i "$o" | gztool -v 0 -f -i -x -C -I "${o%.*}.gzi" | cat
+	fi
+
+	return 0
+}
+
+function helper::papply(){
+	function _usage(){
+		commander::print {COMMANDER[0]}<<- EOF
+			${FUNCNAME[-2]} usage:
+			-l <lines>   | per record. default: 1
+			-t <threads> | number of
+			-o <odir>    | path to. default: TMPDIR
+			-f           | create seekable temporary files. file name is \$FILE
+			-r           | round robin mode i.e. distribute all input evenly across -t jobs. default: initiate new process per chunk
+			-c <cmd>     | and parameters - needs to be last! job id is \$JOB_ID
+
+			example 1:
+			cat file | ${FUNCNAME[-2]} -t 10 -c cat
+
+			example 2:
+			fun(){
+				cat > $JOB_ID.out
+			}
+			export -f fun
+			cat file | ${FUNCNAME[-2]} -t 10 -c fun
+
+			example 3:
+			cat file | ${FUNCNAME[-2]} -f -t 10 -c "cat \$FILE > \$FILE.out"
+
+			example 4:
+			cat file | ${FUNCNAME[-2]} -f -t 10 -c 'cat > $FILE.out'
+		EOF
+		return 1
+	}
+
+	local OPTIND arg mandatory threads=1 l=1 params="" odir="${TMPDIR:-/tmp}"
+	while getopts 'l:t:o:rfc' arg; do
+		case $arg in
+			l)	l=$OPTARG;;
+			t)	threads=$OPTARG;;
+			o)	odir="$OPTARG"; mkdir -p "$odir";;
+			r)	params+=" --round-robin";;
+			f)	params+=" --cat";;
+			c)	((++mandatory)); shift $((OPTIND-1)); break;;
+			*)	_usage;;
+		esac
+	done
+	[[ $mandatory -lt 1 ]] && _usage
+
+	# attention: this solution requires escape sequences within single quotes. (example: cat file | papply -t 10 -c 'cat \> \$JOB_ID.out')
+	# function helper::_papply(){
+	# 	FILE="${1:-/dev/stdin}"
+	# 	JOB_ID="$2" # or $PARALLEL_SEQ
+	# 	shift 2
+	# 	if [[ "$FILE" == "/dev/stdin" ]]; then
+	# 		IFS=$'\n'
+	# 		read -r l < "$FILE" || exit 0
+	# 		cat <(echo "$l") - | eval "$*"
+	# 	else
+	# 		[[ -s "$FILE" ]] || exit 0
+	# 		eval "$*"
+	# 	fi
+	# }
+	# export -f helper::_papply
+	# parallel --tmpdir "$odir" --termseq INT,1000,TERM,0 --halt now,fail=1 --line-buffer --pipe -L $l -N $((5000/l)) $params -P $threads helper::_papply "{}" "{#}" "$@"
+
+	parallel --tmpdir "$odir" --termseq INT,1000,TERM,0 --halt now,fail=1 --line-buffer --pipe -L $l -N $((5000/l)) $params -P $threads "JOB_ID={#}; FILENO={#}; FILE=\"{}\"; if [[ -s \"\$FILE\" ]]; then $*; else read -r l || exit 0; cat <(echo \"\$l\") - | $*; fi"
+
+	return 0
+}
+
+function helper::papply_gzip(){
+	function _usage(){
+		commander::print {COMMANDER[0]}<<- EOF
+			${FUNCNAME[-2]} usage:
+			-i <instances> | number of
+			-f <infile>    | path to
+			-m <modulo>    | chunk size modulo zero. default: 4 (to match fastq file records)
+			-l             | switch from full output buffering and ordered output to immediate line bufferd output
+			-c <cmd>       | and parameters - needs to be last! job id is \$JOB_ID
+
+			example 1:
+			${FUNCNAME[-2]} -i 10 -f file.gz -c cat
+
+			example 2:
+			fun(){
+				cat > $JOB_ID.out
+			}
+			export -f fun
+			${FUNCNAME[-2]} -i 10 -f file.gz -c fun
+
+			example 3:
+			${FUNCNAME[-2]} -i 10 -f file.gz -c "cat > \$JOB_ID.out"
+
+			example 4:
+			${FUNCNAME[-2]} -i 10 -f file.gz -c 'cat > $JOB_ID.out'
+		EOF
+		return 1
+	}
+
+	local OPTIND arg mandatory instances=1 m=4 f tdir="${TMPDIR:-/tmp}" params="--keep-order"
+	while getopts 'i:f:m:lc' arg; do
+		case $arg in
+			i)	instances=$OPTARG;;
+			m)	m=$OPTARG;;
+			l)	params="--line-buffer";;
+			f)	((++mandatory)); f="$OPTARG";;
+			c)	((++mandatory)); shift $((OPTIND-1)); break;;
+			*)	_usage;;
+		esac
+	done
+	[[ $mandatory -lt 2 ]] && _usage
+
+	local l c i
+	l=$(gztool -l "$f" |& sed -nE 's/.*\s+lines\s+:\s+([0-9]+).*/\1/p')
+	c=$((l%instances==0 ? l/instances : l/instances+1))
+	c=$((c%m == 0 ? c : c+m-c%m))
+
+	# attention: this solution requires escape sequences within single quotes. (example: papply_gzip -i 10 -f file.gz -c 'cat \> \$JOB_ID.out')
+	# function helper::_papply_gzip(){
+	# 	JOB_ID="$1"
+	# 	shift
+	# 	eval "$*"
+	# }
+	# export -f helper::_papply_gzip
+	# for i in $(seq 0 $((instances-1))); do
+	# 	echo "gztool -v 0 -L $((i*c+1)) -R $c '$f' |"
+	# done | parallel --tmpdir "$tdir" --termseq INT,1000,TERM,0 --halt now,fail=1 $params -P $instances helper::_papply_gzip "{#}" "{}" "$@"
+
+	# for i in $(seq 0 $((instances-1))); do
+	# 	echo "gztool -v 0 -L $((i*c+1)) -R $c '$f'"
+	# done | parallel --tmpdir "$tdir" --termseq INT,1000,TERM,0 --halt now,fail=1 --keep-order -P $instances '{= $ENV{JOB_ID}=seq() =}' :::: - ::: " | $*"
+	for i in $(seq 0 $((instances-1))); do
+		echo "FILENO=\$PARALLEL_SEQ; JOB_ID=\$PARALLEL_SEQ; gztool -v 0 -L $((i*c+1)) -R $c '$f'"
+	done | parallel --tmpdir "$tdir" --termseq INT,1000,TERM,0 --halt now,fail=1 --keep-order -P $instances {} :::: - ::: " | $*"
 	return 0
 }
 
@@ -126,7 +270,41 @@ function helper::makecatcmd(){
 	[[ $# -eq 0 ]] && { _usage || return 0; }
 	[[ $mandatory -lt 2 ]] && _usage
 
-	_makecatcmd=$({ readlink -e "$f" | file -b --mime-type -f - | grep -oF -e 'gzip' -e 'bzip2' || echo cat; } | sed '/cat/!{s/gzip/pigz -p 1/; s/$/ -cd/}')
+	# pigz p=1: 370MB/s
+	# pigz p=2: 500MB/s
+	# bgzip   : 400MB/s
+	# rapidgzip P=4   : index or bgzip : 1300MB/s | no index: 800MB/s
+	# rapidgzip P=8   : index or bgzip : 1500MB/s | no index: 1100MB/s
+	# _makecatcmd=$({ readlink -e "$f" | file -b --mime-type -f - | grep -oF -e 'gzip' -e 'bzip2' || echo cat; } | sed '/cat/!{s/gzip/pigz -p 2/; s/$/ -kcd/}')
+	# _makecatcmd=$({ readlink -e "$f" | file -b --mime-type -f - | grep -oF -e 'gzip' -e 'bzip2' || echo cat; } | sed '/cat/!{s/gzip/bgzip -@ 1/; s/$/ -kcd/}')
+	# _makecatcmd=$({ readlink -e "$f" | file -b --mime-type -f - | grep -oF -e 'gzip' -e 'bzip2' || echo cat; } | sed '/cat/!{s/gzip/rapidgzip -P 4/; s/$/ -kcd/}')
+
+	case "$(readlink -e "$f" | file -b --mime-type -f - | grep -oF -e 'gzip' -e 'bzip2' || echo cat)" in
+		gzip)
+			local params
+			if [[ -e "${f%.*}.rgzi" ]]; then
+				params="-P 4 --import-index '"${f%.*}.rgzi"'"
+			else
+				# bgzip || gzip
+				params="-P $(gzip -l "$f" | tail -1 | awk '$1>0 && $2==0{print 4; exit}{print 8}')"
+			fi
+			local d=$(
+				d="$(df --output=source "$(readlink -e "$f")" | tail -1)"
+				lsblk -n -o ROTA "$d" 2> /dev/null || {
+					d="$(realpath "/dev/disk/by-label/$(rev <<< "$d" | xargs basename | rev)")"
+					lsblk -n -o ROTA "$d" 2> /dev/null || echo 0
+				}
+			)
+			if [[ $d -eq 1 ]]; then
+				params+=" --io-read-method sequential"
+			fi
+			_makecatcmd="rapidgzip -kcd $params"
+		;;
+		bzip2)
+			_makecatcmd="bzip2 -kcd"
+			;;
+		*)	_makecatcmd="cat";;
+	esac
 
 	return 0
 }
